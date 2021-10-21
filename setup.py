23916ae4bbb38a425a9d5ceb29aa1daa4be552e4
mdrosiadis <42861519+mdrosiadis@users.noreply.github.com>
2021-10-21 20:00:27 +0300

    Fixed merge remains on setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-from setuptools import find_packages, setup
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setup(
-    name="pgbackup",
-    version="0.1.0",
-    author="Angelos Anagnostopoulos",
-    author_email="angelosanag@protonmail.com",
-    description="A utility for backing up PostgreSQL databases.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/AngelosAnagnostopoulos/pgbackup",
-    packages=find_packages("src")
-        )
-=======
 from setuptools import setup, find_packages
 
 with open('README.rst', encoding = 'utf-8') as f:
@@ -35,5 +17,3 @@ setup(
         'console_scripts':'hr=hr.cli:main'
     }
 )
->>>>>>> 3dd6d3d (Done)
-
